92d9dbcdb27c67b62ccf01de3f24cfd1df239acb
quzzi21 <99156797+quzzi21@users.noreply.github.com>
2023-03-11 10:28:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+$('#username, #password').on('input', function() {
+  if ($('#username').val() && $('#password').val()) {
+      $('.login').addClass('buttonafter');
+  } else {
+      $('.login').removeClass('buttonafter');
+  }
+});
